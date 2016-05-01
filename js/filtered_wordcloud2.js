092bc0b2d03014300849362061f8d621f94839ae
Annie Schugart <annieschugart@Annies-MacBook-Pro-2.local>
2016-05-01 12:31:32 -0400

    word cloud works

--- js/filtered_wordcloud2.js
+++ js/filtered_wordcloud2.js
@@ -1,21 +1,26 @@
 var border = 1;
 var bordercolor='black';
 
-// var frequency_list = [{"text":"study","size":40},{"text":"motion","size":15},{"text":"forces","size":10}];
+// var frequency_list = [{"text":"study","size":40},{"text":"motion","size":15},{"text":"forces","size":10}];   
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width_wordcloud = 500 - margin.left - margin.right,
     height_wordcloud = 350 - margin.top - margin.bottom;
 
+// var fisheye = d3.fisheye.circular()
+//     .radius(200)
+//     .distortion(2);
+
 var svg_wordcloud2 = d3.select("#wordcloud2").append("svg")
     .attr("width", width_wordcloud + margin.left + margin.right)
     .attr("height", height_wordcloud + margin.top + margin.bottom)
-    .attr("class", "wordcloud2")
+    .attr("class", "wordcloud1")
     .attr("style", "outline: thin solid black;")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var data2;
+
 
 loadData2();
 
@@ -28,7 +33,7 @@ function loadData2() {
 };
 
 // Default values for wordcloud1
-var selected2_gender = "1";
+var selected2_gender = "0";
 var selected2_pcsb = "all_pcsb";
 var selected2_race = "all_race";
 
@@ -38,18 +43,17 @@ function UpdateVisualization2() {
     selected2_pcsb = d3.select('input[name="pcsb2option"]:checked').node().value
     selected2_race = d3.select('input[name="race2option"]:checked').node().value
 
-    // filter the array
-    var gender_filter2;
-    var pcsb_filter2;
-    var race_filter2;
-
     var tip = d3.select('#wordcloud2')
     .append('div')
     .attr('class', 'd3-tip-cloud')
 
-    tip.append('div')
+   tip.append('div')
     .attr('class', 'text');
 
+    // filter the array
+    var gender_filter2;
+    var pcsb_filter2;
+    var race_filter2;
 
     gender_filter2 = data2.filter(function (d) {
         if (selected2_gender == "all_gender") {
@@ -96,28 +100,41 @@ function UpdateVisualization2() {
     word_frequencies2 = d3.values(word_frequencies2);
     console.log(word_frequencies2);
 
+    var greyscale2 = ["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"];
+
     var color2 = d3.scale.linear()
         .domain([0,1,2,3,4,5,6,10,15,20,100])
-        .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
+        .range(greyscale2);
 
     d3.layout.cloud().size([700, 300])
         .words(word_frequencies2)
         .rotate(0)
-        .fontSize(function(d) { return d.size + 10; })
+        .fontSize(function(d) { 
+            return (d.size + 10); 
+        })
         .on("end", draw)
+
         .start();
 
-    function draw(words) {
-        var wordcloud = svg_wordcloud2
+
+
+    // var wordcloud = svg_wordcloud1
+    //         // without the transform, words words would get cutoff to the left and top, they would
+    //         // appear outside of the SVG area
+    //         .attr("transform", "translate(200,200)");
+            
+
+    function draw(words2) {
+        var wordcloud2 = svg_wordcloud2
             // without the transform, words words would get cutoff to the left and top, they would
             // appear outside of the SVG area
             .attr("transform", "translate(200,200)")
             .selectAll("text")
-            .data(words);
+            .data(words2);
 
-        wordcloud.enter().append("text");
+        wordcloud2.enter().append("text");
 
-        wordcloud
+        wordcloud2
             .style("font-size", function(d) {
                 return d.size + "px"; 
             })
@@ -129,17 +146,17 @@ function UpdateVisualization2() {
             })
             .text(function(d) { return d.text; });
 
-        wordcloud
+        wordcloud2
             .on('mouseover', function(d) {
                 tip.select('.text').html("Word: " + d.text + "<br>Occurrences: " + (d.size - 10) + " ("
-                 + (((d.size - 10)/(filteredData.length)) * 100).toFixed(2) + "%)");
+                 + (((d.size - 10)/(filteredData2.length)) * 100).toFixed(2) + "%)");
                 tip.style('display', 'block');
             })
             .on('mouseout', function() {
                 tip.style('display', 'none');
             });
 
-        wordcloud.exit()
+        wordcloud2.exit()
         .transition()
         .duration(1000)
         .remove();
